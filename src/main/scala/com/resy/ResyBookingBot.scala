69925cc0eb5f1754e54b679a54f6ae2d4cb2929c
Alkaar <Alkaar@gmail.com>
2018-02-19 02:39:53 -0500

    Fixed sbt version bug and added reservation day request.

--- src/main/scala/com/resy/ResyBookingBot.scala
+++ src/main/scala/com/resy/ResyBookingBot.scala
@@ -1,12 +1,14 @@
 package com.resy
 
+import play.api.routing.sird.&
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object ResyBookingBot {
   def main(args: Array[String]): Unit = {
     //STEP 1: GET PAYMENT ID
-    val userDetailsResp = ResyApiWrapper.sendRequest(ResyApiMapKeys.UserDetails, Map.empty, false)
+    val userDetailsResp = ResyApiWrapper.sendRequest(ResyApiMapKeys.UserDetails, Map.empty, true)
 
     Await.result(userDetailsResp, 5000 millis)
 
@@ -26,6 +28,22 @@ object ResyBookingBot {
 
     println(s"Payment Method Id: ${paymentMethodId}")
 
+    //STEP 1: GET RESERVATION
+    val queryParams = Map(
+      "day" -> "2018-03-21",
+      "lat" -> "0",
+      "long" -> "0",
+    "party_size" -> "2",
+    "venue_id" -> "834")
+
+    val reservationResp = ResyApiWrapper.sendRequest(ResyApiMapKeys.FindReservation, queryParams, true)
+
+    Await.result(reservationResp, 5000 millis)
+
+    val reservationDetails = reservationResp.value.get.get
+    println(s"URL Response: $reservationDetails")
+
     println("Done")
+    System.exit(1)
   }
 }
