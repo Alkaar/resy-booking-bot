7da564f49638f39a138bd0a9b4587560a3e825cb
Alkaar <Alkaar@gmail.com>
2018-04-23 21:21:36 -0400

    Redesign of reservation workflow to be more maintainable
    * Changed to use JSON parsing instead of pattern matching
    * Change futures to use the for comprehension
    * Added retry to the first API call.
    * General code clean up

---
+++ src/main/scala/com/resy/BookReservationWorkflow.scala
@@ -0,0 +1,114 @@
+package com.resy
+
+import com.resy.BookingDetails.{time, _}
+import com.resy.ResyApiWrapper._
+import org.joda.time.DateTime
+import play.api.libs.json.JsResult.Exception
+import play.api.libs.json.{JsArray, JsError, Json}
+
+import scala.annotation.tailrec
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success, Try}
+
+object BookReservationWorkflow {
+  implicit val testing = false
+
+  /**
+    * STEP 1: FIND RESERVATION (GET CONFIG ID)
+    * @return
+    */
+  private[this] def findReservation: Future[String] = {
+    val findResQueryParams = Map(
+      "day" -> day,
+      "lat" -> "0",
+      "long" -> "0",
+      "party_size" -> partySize,
+      "venue_id" -> venueId)
+
+    sendGetRequest(ResyApiMapKeys.FindReservation, findResQueryParams)
+  }
+
+  /**
+    * STEP 2: GET RESERVATION DETAILS (GET PAYMENT ID AND BOOK TOKEN)
+    * @param configId
+    * @return
+    */
+  def getReservationDetails(configId: String) = {
+    val findResQueryParams = Map(
+      "config_id" -> configId,
+      "day" -> day,
+      "party_size" -> partySize)
+
+    sendGetRequest(ResyApiMapKeys.ReservationDetails, findResQueryParams)
+  }
+
+  /**
+    * STEP 3: BOOK RESERVATION
+    * @param resDetailsResp
+    * @return
+    */
+  def bookReservation(resDetailsResp: String) = {
+    val resDetails = Json.parse(resDetailsResp)
+    println(s"${DateTime.now} URL Response: $resDetailsResp")
+
+    //PaymentMethodId - Searching for this pattern - "payment_methods": [{"is_default": true, "provider_name": "braintree", "id": 123456, "display": "1234", "provider_id": 1}]
+    val paymentMethodId =
+      (resDetails \ "user" \ "payment_methods" \ 0 \ "id")
+        .get
+        .toString
+
+    println(s"${DateTime.now} Payment Method Id: $paymentMethodId")
+
+    //BookToken - Searching for this pattern - "book_token": {"value": "book_token_value"
+    val bookToken =
+      (resDetails \ "book_token" \ "value")
+        .get
+        .toString
+        .stripPrefix("\"")
+        .stripSuffix("\"")
+
+    println(s"${DateTime.now} Book Token: $bookToken")
+
+    val bookResQueryParams = Map(
+      "book_token" -> bookToken,
+      "struct_payment_method" -> s"""{"id":$paymentMethodId}""",
+      "source_id" -> "resy.com-venue-details")
+
+    sendPostRequest(ResyApiMapKeys.BookReservation, bookResQueryParams)
+  }
+
+  /**
+    * Same as Step 1 but does a retry.  Blocks because the reservation can't proceed without an available reservation
+    * @param endTime
+    * @return
+    */
+  @tailrec
+  def retryFindReservation(endTime: Long): String = {
+    val findResResp = Await.result(findReservation, 5 seconds)
+
+    println(s"${DateTime.now} URL Response: $findResResp")
+
+    //ConfigId - Searching for this pattern - "time_slot": "17:15:00", "badge": null, "service_type_id": 2, "colors": {"background": "2E6D81", "font": "FFFFFF"}, "template": null, "id": 123457
+    val reservation =
+      Try(((Json.parse(findResResp) \ "results" \ 0 \ "configs")
+        .get
+        .as[JsArray]
+        .value
+        .filter(x => (x \ "time_slot").get.toString == s""""$time"""")
+        (0) \ "id")
+        .get
+        .toString
+      )
+
+    reservation match {
+      case Success(configId) =>
+        println(s"${DateTime.now} Config Id: $configId")
+        configId
+      case Failure(_) if endTime - DateTime.now.getMillis > 0 =>
+        retryFindReservation(endTime)
+      case _ =>
+        throw new Exception(JsError("Could not find a reservation for the given time"))
+    }
+  }
+}
--- src/main/scala/com/resy/ResyBookingBot.scala
+++ src/main/scala/com/resy/ResyBookingBot.scala
@@ -1,20 +1,20 @@
 package com.resy
 
 import akka.actor.ActorSystem
-import com.resy.BookingDetails._
+import com.resy.BookReservationWorkflow._
 import org.joda.time.DateTime
 import play.api.libs.json._
 
-import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 object ResyBookingBot {
   def main(args: Array[String]): Unit = {
     println("Starting Resy Booking Bot")
 
     val system = ActorSystem("System")
-    val startOfTomorrow = DateTime.now.withTimeAtStartOfDay.plusDays(1).plusSeconds(1).getMillis
+    val startOfTomorrow = DateTime.now.withTimeAtStartOfDay.plusDays(1).getMillis
     val millisUntilTomorrow = startOfTomorrow - DateTime.now.getMillis
     val hoursRemaining = millisUntilTomorrow/1000/60/60
     val minutesRemaining = millisUntilTomorrow/1000/60 - hoursRemaining * 60
@@ -22,99 +22,38 @@ object ResyBookingBot {
 
     println(s"Current time: ${DateTime.now}")
     println(s"Sleeping for $hoursRemaining hours, $minutesRemaining minutes and $secondsRemaining seconds")
-    system.scheduler.scheduleOnce(millisUntilTomorrow millis)(bookReservation)
+    system.scheduler.scheduleOnce(millisUntilTomorrow millis)(bookReservationWorkflow)
   }
 
-  def bookReservation = {
-    println("Attempting to snipe reservation at " + DateTime.now)
-    //STEP 1: FIND RESERVATION (GET CONFIG ID)
-    val findResQueryParams = Map(
-      "day" -> day,
-      "lat" -> "0",
-      "long" -> "0",
-      "party_size" -> partySize,
-      "venue_id" -> venueId)
+  def bookReservationWorkflow = {
+    println(s"Attempting to snipe reservation at ${DateTime.now}")
 
-    val findResResp = ResyApiWrapper.sendGetRequest(ResyApiMapKeys.FindReservation, findResQueryParams, false)
+    //Try to get configId of the time slot for 10 seconds
+    val findResResp = retryFindReservation(DateTime.now.plusSeconds(10).getMillis)
 
-    Await.result(findResResp, 5000 millis)
+    //Try to book the reservation
+    for(resDetailsResp <- getReservationDetails(findResResp);
+        bookResResp <- bookReservation(resDetailsResp)
+    ) {
+      val resyToken =
+        Try((Json.parse(bookResResp) \ "resy_token")
+          .get
+          .toString
+          .stripPrefix("\"")
+          .stripSuffix("\"")
+        )
 
-    val reservations = Json.parse(findResResp.value.get.get)
-    println(s"${DateTime.now} URL Response: $reservations")
+      resyToken match {
+        case Success(token) =>
+          println(s"Successfully sniped reservation at ${DateTime.now}")
+          println(s"Resy token is $token")
+        case Failure(error) =>
+          println(s"Couldn't sniped reservation at ${DateTime.now}")
+          println(s"Error message is $error")
+      }
 
-    //ConfigId - Searching for this pattern - "time_slot": "17:15:00", "badge": null, "service_type_id": 2, "colors": {"background": "2E6D81", "font": "FFFFFF"}, "template": null, "id": 123457
-    val configId =
-      ((reservations \ "results" \ 0 \ "configs")
-        .get
-        .as[JsArray]
-        .value
-        .filter(x => (x \ "time_slot").get.toString == s""""$time"""")
-        (0) \ "id")
-        .get
-        .toString
-
-    println(s"${DateTime.now} Config Id: ${configId}")
-
-    //STEP 2: GET RESERVATION DETAILS (GET PAYMENT ID AND BOOK TOKEN)
-    val resDetailsQueryParams = Map(
-      "config_id" -> configId,
-      "day" -> day,
-      "party_size" -> partySize)
-
-    val resDetailsResp = ResyApiWrapper.sendGetRequest(ResyApiMapKeys.ReservationDetails, resDetailsQueryParams, false)
-
-    Await.result(resDetailsResp, 5000 millis)
-
-    val resDetails = resDetailsResp.value.get.get
-    println(s"${DateTime.now} URL Response: $resDetails")
-
-    //PaymentMethodId - Searching for this pattern - "payment_methods": [{"is_default": true, "provider_name": "braintree", "id": 123456, "display": "1234", "provider_id": 1}]
-    val paymentPattern = """"payment_methods"[\s]*:[\s]*\[.+?\]""".r
-    val paymentKeyPattern = """"id"[\s]*:[\s]*\d+""".r
-    val paymentValuePattern = """\d+$""".r
-
-    val paymentMethodId =
-      paymentValuePattern
-        .findFirstIn(paymentKeyPattern
-          .findFirstIn(
-            paymentPattern
-              .findFirstIn(resDetails)
-              .get)
-          .get)
-        .get
-
-    println(s"${DateTime.now} Payment Method Id: ${paymentMethodId}")
-
-    //BookToken - Searching for this pattern - "book_token": {"value": "book_token_value"
-    val bookTokenPattern = """"book_token"[\s]*:[\s]*.+?"value"[\s]*:[\s]*"[^"]+"""".r
-    val bookTokenKeyPattern = """"value"[\s]*:[\s]*"[^"]+""".r
-    val bookTokenValuePattern = """[^"]+$""".r
-
-    val bookToken =
-      bookTokenValuePattern
-        .findFirstIn(bookTokenKeyPattern
-          .findFirstIn(bookTokenPattern
-            .findFirstIn(resDetails)
-            .get)
-          .get)
-        .get
-
-    println(s"${DateTime.now} Book Token: ${bookToken}")
-
-    //STEP 3: BOOK RESERVATION
-    val bookResQueryParams = Map(
-      "book_token" -> bookToken,
-      "struct_payment_method" -> s"""{"id":$paymentMethodId}""",
-      "source_id" -> "resy.com-venue-details")
-
-    val bookResResp = ResyApiWrapper.sendPostRequest(ResyApiMapKeys.BookReservation, bookResQueryParams, false)
-
-    Await.result(bookResResp, 10000 millis)
-
-    val bookResDetails = bookResResp.value.get.get
-    println(s"${DateTime.now} URL Response: $bookResDetails")
-
-    println("Shutting down Resy Booking Bot at " + DateTime.now)
-    System.exit(1)
+      println("Shutting down Resy Booking Bot at " + DateTime.now)
+      System.exit(0)
+    }
   }
 }
