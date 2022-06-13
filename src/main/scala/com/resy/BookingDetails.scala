b98a2a2748424998ee9f36b66e118e7c27103c85
nynda <nynda@me.com>
2022-06-12 20:22:01 -0500

    Update BookingDetails.scala
    details book

--- src/main/scala/com/resy/BookingDetails.scala
+++ src/main/scala/com/resy/BookingDetails.scala
@@ -2,16 +2,19 @@ package com.resy
 
 object BookingDetails {
   // Your user profile Auth Token
-  val auth_token = ""
+  val auth_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NTg5NzA0MzksInVpZCI6MzIxNTEyMDAsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMTM1NTU0NTh9fQ.ABlv34Puy0XiWmP_VxiOt0pGf3Xf-c72I8aKH-Ov2Bv5VactO-pJThkgXtKle217Uyj2ceoOiRAkVXRVluGwXpZgABtEFRMEPnUKrgqG-N8z6vSIVrQ5nP1ZfDbWMpDxLKtAcD0F3JB3AmRv--tbCK1aHdrBjiqVGgrN_d2F_5Ho3VJK"
   // Your user profile API key
-  val api_key = ""
+  val api_key = "VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"
   // RestaurantId where you want to make the reservation
-  val venueId = ""
+  val venueId = "6194"
   // YYYY-MM-DD of reservation
-  val day = "2018-01-01"
-
+  val day = "2022-07-13"
   // Seq of HH:MM:SS times of reservations in military time format
   val times = Seq(
+  "17:00:00",
+    "17:15:00",
+    "17:30:00",
+    "17:45:00",
     "18:00:00",
     "18:15:00",
     "18:30:00",
