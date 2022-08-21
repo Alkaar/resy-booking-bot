48c84b6c3486a5ef8d1f6813b17e327cac5b9e6f
Derek Fong <Alkaar@gmail.com>
2022-08-20 20:39:25 -0400

    feat: allow retrying snipes (#34)

--- src/test/scala/com/resy/ResyBookingWorkflowSpec.scala
+++ src/test/scala/com/resy/ResyBookingWorkflowSpec.scala
@@ -33,6 +33,40 @@ class ResyBookingWorkflowSpec extends AnyFlatSpec with Matchers {
     ResyBookingWorkflow.run(resyClient, resDetails) shouldEqual Success("RESY_TOKEN")
   }
 
+  it should "find and book an available reservation with retrying" in new Fixture {
+    when(resyApi.getReservations(resDetails.date, resDetails.partySize, resDetails.venueId))
+      .thenReturn(Future(Source.fromResource("getReservations.json").mkString))
+
+    when(resyApi.getReservationDetails(configId, resDetails.date, resDetails.partySize))
+      .thenReturn(Future(Source.fromResource("getReservationDetails.json").mkString))
+
+    when(resyApi.postReservation(paymentMethodId, bookToken))
+      .thenReturn(Future(""))
+      .thenReturn(Future(Source.fromResource("bookReservation.json").mkString))
+
+    ResyBookingWorkflow.run(resyClient, resDetails) shouldEqual Success("RESY_TOKEN")
+
+    verify(resyApi, Mockito.times(2))
+      .getReservations(
+        date      = resDetails.date,
+        partySize = resDetails.partySize,
+        venueId   = resDetails.venueId
+      )
+
+    verify(resyApi, Mockito.times(2))
+      .getReservationDetails(
+        configId  = configId,
+        date      = resDetails.date,
+        partySize = resDetails.partySize
+      )
+
+    verify(resyApi, Mockito.times(2))
+      .postReservation(
+        paymentMethodId = paymentMethodId,
+        bookToken       = bookToken
+      )
+  }
+
   it should "find but fail to book an available reservation" in new Fixture {
     when(resyApi.getReservations(resDetails.date, resDetails.partySize, resDetails.venueId))
       .thenReturn(Future(Source.fromResource("getReservations.json").mkString))
@@ -43,7 +77,7 @@ class ResyBookingWorkflowSpec extends AnyFlatSpec with Matchers {
     when(resyApi.postReservation(paymentMethodId, bookToken))
       .thenReturn(Future(""))
 
-    ResyBookingWorkflow.run(resyClient, resDetails) match {
+    ResyBookingWorkflow.run(resyClient, resDetails, 0) match {
       case Failure(exception) =>
         withClue("RuntimeException not found:") {
           exception.isInstanceOf[RuntimeException] shouldEqual true
