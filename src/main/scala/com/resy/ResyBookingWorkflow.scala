9401dbe114adaa0bc72bbdc225e2acc5ec8e0b1c
Alkaar <alkaar@gmail.com>
2022-08-20 20:34:12 -0400

    retry if failed to snipe

--- src/main/scala/com/resy/ResyBookingWorkflow.scala
+++ src/main/scala/com/resy/ResyBookingWorkflow.scala
@@ -1,20 +1,28 @@
 package com.resy
 
 import org.apache.logging.log4j.scala.Logging
+import org.joda.time.DateTime
 
-import scala.util.Try
+import scala.annotation.tailrec
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.{Failure, Try}
 
 object ResyBookingWorkflow extends Logging {
 
+  @tailrec
   def run(
     resyClient: ResyClient,
-    resDetails: ReservationDetails
+    resDetails: ReservationDetails,
+    millisToRetry: Long = (10 seconds).toMillis
   ): Try[String] = {
+    val dateTimeStart = DateTime.now.getMillis
+
     logger.info("Taking the shot...")
     logger.info("(҂‾ ▵‾)︻デ═一 (˚▽˚’!)/")
     logger.info(s"Attempting to snipe reservation")
 
-    for {
+    val maybeResyTokenResp = for {
       configId <- resyClient.retryFindReservation(
         date         = resDetails.date,
         partySize    = resDetails.partySize,
@@ -31,5 +39,13 @@ object ResyBookingWorkflow extends Logging {
         bookingDetails.bookingToken
       )
     } yield resyToken
+
+    val timeLeftToRetry = millisToRetry - (DateTime.now.getMillis - dateTimeStart)
+
+    maybeResyTokenResp match {
+      case Failure(_) if timeLeftToRetry > 0 =>
+        run(resyClient: ResyClient, resDetails, timeLeftToRetry)
+      case maybeResyToken => maybeResyToken
+    }
   }
 }
